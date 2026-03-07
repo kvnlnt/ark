1f2f0f66efb751a34ccde16046bfe545271962da
Kevin Lint <kevin@concreit.com>
2026-03-07 10:20:53 -0800

    pre-hook lint test

--- ark.ts
+++ ark.ts
@@ -3,3 +3,11 @@ import config from "./ark.json";
 import { type CliConfig, runCli } from "./components/Cli.ts";
 
 runCli(config as CliConfig, import.meta.path);
+
+let unusedVar = 42;
+
+function greet(name) {
+	console.log("Hello " + name);
+}
+
+greet("World");
